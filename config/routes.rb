d58fe8bc29b615471771e4920aaec9f580508d7c
alegscogs <alex@kickstarter.com>
2018-01-20 14:53:18 +0100

    Route root url to site/home
    (cherry picked from commit 1b6a6afcbaa13384904b7431e858e9381803fc77)

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
+
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  get 'site/home'
-
   resources :posts
   resources :users
 
+  get 'site/home'
+  root 'site#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
